398650dca1370aa2abf4c30dd888b2ced8017a1b
iofirag <iofirag@gmail.com>
2026-04-19 13:16:30 +0300

    Update: is-palindrome - submission-0

--- Data Structures & Algorithms/is-palindrome/submission-0.py
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -4,21 +4,18 @@ class Solution:
         left_i = 0
         right_i = len(s)-1
         while left_i < right_i:
-            # check if both 
             if not s[left_i].isalnum():
                 # char not in ascii of A-Z a-z 0-9
                 left_i += 1
-                continue
-
-            if not s[right_i].isalnum():
+            elif not s[right_i].isalnum():
+                # char not in ascii of A-Z a-z 0-9
                 right_i -= 1
-                continue
-
-            if s[left_i].lower() == s[right_i].lower():
+            elif s[left_i].lower() != s[right_i].lower():
+                # both chars but not match
+                return False
+            else:
                 left_i += 1
                 right_i -= 1
                 continue
-            else:
-                return False
             
         return True
